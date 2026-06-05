e7388b6d8a72c998ce3a7925f2cc38159b696412
Pushkar Raj <pushkarcodes10@gmail.com>
2026-06-05 12:19:55 +0530

    learnt for each loop

--- JavaDay3/Main.java
+++ JavaDay3/Main.java
@@ -1,7 +1,12 @@
 /*
 package JavaDay3;
 
-import java.net.CacheRequest;
+// class Student 
+// {
+//     int rollno;
+//     String name;
+//     int marks;
+// }
 
 class Calculator {
     int num = 5;
@@ -115,6 +120,50 @@ public class Main {
             }
             System.out.println();
         }
+
+             // Student s1 = new Student();
+        // s1.rollno = 25;
+        // s1.name = "Pushkar";
+        // s1.marks = 77;
+
+        // Student s2 = new Student();
+        // s2.rollno = 26;
+        // s2.name = "Harsh";
+        // s2.marks = 87;
+
+        // Student s3 = new Student();
+        // s3.rollno = 27;
+        // s3.name = "Kiran";
+        // s3.marks = 97;
+
+        // Student students[] = new Student[3];
+        // students[0] = s1;
+        // students[1] = s2;
+        // students[2] = s3;
+
+        // for(int i=0;i<students.length;i++) {
+        //     System.out.println(students[i].name + " : " + students[i].marks);
+        // }
+
+        // for(Student stud : students)
+        // {
+        //     System.out.println(stud.name + " : " + stud.marks);
+        // }
+
+        // int nums[] = new int[4];
+        // nums[0] = 4;
+        // nums[1] = 8;
+        // nums[2] = 3;
+        // nums[3] = 9;
+
+        // for(int i=0;i<nums.length;i++) {
+        //     System.out.println(nums[i]);
+        // }
+
+    //     for(int n : nums)
+    //     {
+    //         System.out.println(n);
+    //     }
 
     }
 }
